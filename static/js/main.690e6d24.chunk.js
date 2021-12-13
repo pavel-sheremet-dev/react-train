402913263b5e28d09daf69215094a9bbf3a815b9
Pavel Sheremet <pashqa.sheremet@gmail.com>
2021-12-13 17:44:24 +0200

    Updates

---
+++ static/js/main.690e6d24.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpreact-train"]=this["webpackJsonpreact-train"]||[]).push([[0],{11:function(t,n,e){"use strict";e.r(n);var r=e(1),c=e.n(r),i=e(3),o=e.n(i),a=(e(8),e(9),e(0));var s=function(){return Object(a.jsx)("div",{children:"Hello world"})};o.a.render(Object(a.jsx)(c.a.StrictMode,{children:Object(a.jsx)(s,{})}),document.getElementById("root"))},8:function(t,n,e){},9:function(t,n,e){}},[[11,1,2]]]);
+//# sourceMappingURL=main.690e6d24.chunk.js.map
